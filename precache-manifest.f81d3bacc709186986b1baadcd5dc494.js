d112c92f085b6d731763d5baa77d637758599e11
ts <tsumrow@gmail.com>
2020-01-14 12:06:20 -0800

    Updates

---
+++ precache-manifest.f81d3bacc709186986b1baadcd5dc494.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "835d4ef20bf97e1c922b296c252f9668",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "19cdff1005abfd8dca9a",
+    "url": "/my-app/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "8af1c97d36f0b1426ae8",
+    "url": "/my-app/static/js/2.b6fabe4d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/my-app/static/js/2.b6fabe4d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "19cdff1005abfd8dca9a",
+    "url": "/my-app/static/js/main.045f5d78.chunk.js"
+  },
+  {
+    "revision": "134c6d7b033e4b816dc9",
+    "url": "/my-app/static/js/runtime-main.56c001b7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
